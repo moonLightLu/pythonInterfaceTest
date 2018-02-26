81ffa44098ed33769623ebad7973c889a0087a6f
zengweiting <yingyue793@163.com>
2018-02-26 20:44:20 +0800

    test update

--- cookie/cookieSend.py
+++ cookie/cookieSend.py
@@ -4,6 +4,17 @@
 import requests
 import json
 
-url = "http://httpbin.org/post"
+url = "http://httpbin.org/get"
 
 #方法1：简单发送
+#cookies = {"aaa":"bbb"}
+#r = requests.get(url,cookies = cookies)
+#print r.text
+
+#方法2：复杂发送
+s = requests.session()
+c = requests.cookies.RequestsCookieJar()
+c.set('c-name', 'c-value',path = '/xxx/uuu',domain = '.test.com')
+s.cookies.update(c)
+r = requests.get(url,cookies = c)
+print r.text
